e55c3dc1290ad8e166cd76870c7d5374599286f1
Huub <huubhoofs@gmail.com>
2018-07-19 23:02:30 +0200

    theme_bw()

--- plots.r
+++ plots.r
@@ -1,8 +1,17 @@
 require(ggplot2)
 require(foreign)
 
-df_ <- read.csv('db_nist.csav', stringsAsFactors = FALSE)
+df_ <- read.csv('db_nist.csv', stringsAsFactors = FALSE)
 df_$gender <- as.factor(df_$gender)
 df_$pattern <- as.factor(df_$pattern)
 
-ggplot(data=df_, aes(df_$pattern, fill=df_$pattern)) + geom_bar(colour='black')
+plot_patterns <-
+  ggplot(data=df_, aes(pattern, fill=pattern)) +
+  geom_bar(colour='black') +
+  scale_x_discrete('Patterns') + scale_y_continuous('Frequency (n)') +
+  scale_fill_grey() +
+  theme_bw() +
+  theme(legend.position = 'none')
+
+
+ggsave('figs/plot_patterns.png', plot=plot_patterns)
